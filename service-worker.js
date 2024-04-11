616f1d33ecb6f6b5eace1a67b3b7a3772649b651
Froilan Delfin Jr <142400742+FroilanDelfinJr@users.noreply.github.com>
2024-04-11 12:51:06 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,4 @@
 // Allows users to open the side panel by clicking on the action toolbar icon
-chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch((error) => console.error(error));
-
 const welcomePage = "main/welcomePage.html";
 
 chrome.runtime.onInstalled.addListener(() => {
